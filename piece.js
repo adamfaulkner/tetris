94a8141194c6e7d3f5d62f03cea5fccfb4628491
Adam Faulkner <adamfaulkner1@gmail.com>
2012-10-04 22:36:40 -0500

    Moved piece code into separate module'

---
+++ piece.js
@@ -0,0 +1,79 @@
+define('piece', [], function() {
+	var pieces = [
+		// long piece
+		[
+			['#00F'],
+			['#00F'],
+			['#00F'],
+			['#00F']
+		],
+		// square
+		[
+			['#FF0', '#FF0'],
+			['#FF0', '#FF0']
+		],
+		// T
+		[
+			['#FFF', '#F00', '#FFF'],
+			['#F00', '#F00', '#F00']
+		],
+		// squigle thing one
+		[
+			['#FFF', '#F0F', '#F0F'],
+			['#F0F', '#F0F', '#FFF']
+		],
+		// squigle thing two
+		[
+			['#0F0', '#0F0', '#FFF'],
+			['#FFF', '#0F0', '#0F0']
+		]
+	];
+
+	var make_rotates = function(piece, rotates) {
+		var new_piece = [];
+		// Loop through the rows, columns of the NEW piece
+		for (var i = 0; i < piece[0].length; i++) {
+			var new_row = [];
+			for (var j = 0; j < piece.length; j++) {
+				new_row.push(piece[piece.length - j - 1][i]);
+			}
+			new_piece.push(new_row);
+		}
+		if (rotates.length >= 4) {
+			return rotates;
+		} else {
+			rotates.push(new_piece);
+			return make_rotates(new_piece, rotates);
+		}
+	};
+
+	// Choose a random piece
+	var pick_piece = function(px, gwidth) {
+		var index = Math.floor(Math.random() * pieces.length);
+		piece = pieces[index];
+		piece_rotates = make_rotates(piece, [piece]);
+		current_rotation = 0;
+		px = Math.min(px, gwidth - piece[0].length);
+	};
+
+	var rotate = function() {
+		current_rotation = ++current_rotation % piece_rotates.length;
+		piece = piece_rotates[current_rotation];
+	};
+
+	// Index into the pieces array
+	var piece;
+	var piece_rotates;
+	// Integer referring to current rotation
+	var current_rotation;
+	pick_piece();
+
+	var get_piece = function() {
+		return piece;
+	};
+
+	return { piece: get_piece,
+					 pick_piece: pick_piece,
+					 rotate: rotate
+				 };
+});
--- state.js
+++ state.js
@@ -1,5 +1,5 @@
 // Set up the global state
-define('state', [], function() {
+define('state', ['piece'], function(Piece) {
 	function Game(canvas) {
 		var board = [];
 		canvas.width = 300;
@@ -17,69 +17,7 @@ define('state', [], function() {
 		}
 		var px = 0;
 		var py = 0;
-		var pieces = [
-			// long piece
-			[
-				['#00F'],
-				['#00F'],
-				['#00F'],
-				['#00F']
-			],
-			// square
-			[
-				['#FF0', '#FF0'],
-				['#FF0', '#FF0']
-			],
-			// T
-			[
-				['#FFF', '#F00', '#FFF'],
-				['#F00', '#F00', '#F00']
-			],
-			// squigle thing one
-			[
-				['#FFF', '#F0F', '#F0F'],
-				['#F0F', '#F0F', '#FFF']
-			],
-			// squigle thing two
-			[
-				['#0F0', '#0F0', '#FFF'],
-				['#FFF', '#0F0', '#0F0']
-			]
-		];
 
-		var make_rotates = function(piece, rotates) {
-			var new_piece = [];
-			// Loop through the rows, columns of the NEW piece
-			for (var i = 0; i < piece[0].length; i++) {
-				var new_row = [];
-				for (var j = 0; j < piece.length; j++) {
-					new_row.push(piece[piece.length - j - 1][i]);
-				}
-				new_piece.push(new_row);
-			}
-			if (rotates.length >= 4) {
-				return rotates;
-			} else {
-				rotates.push(new_piece);
-				return make_rotates(new_piece, rotates);
-			}
-		};
-
-		// Choose a random piece
-		var pick_piece = function() {
-			var index = Math.floor(Math.random() * pieces.length);
-			piece = pieces[index];
-			piece_rotates = make_rotates(piece, [piece]);
-			current_rotation = 0;
-			px = Math.min(px, gwidth - piece[0].length);
-		};
-
-		// Index into the pieces array
-		var piece;
-		var piece_rotates;
-		// Integer referring to current rotation
-		var current_rotation;
-		pick_piece();
 		var draw = function() {
 			var bwidth = width / gwidth;
 			var bheight = height / gheight;
@@ -98,7 +36,7 @@ define('state', [], function() {
 			});
 
 			// Don't do any collision detection here, just draw it
-			piece.forEach(function(row, i) {
+			Piece.piece().forEach(function(row, i) {
 				row.forEach(function(cell, j) {
 					// This is dumb, don't do this
 					if (cell != '#FFF') {
@@ -123,9 +61,9 @@ define('state', [], function() {
 				py -= 1;
 				place = true;
 			}
-			place = place || py + piece.length >= gheight;
+			place = place || py + Piece.piece().length >= gheight;
 			if (place) {
-				piece.forEach(function(row, i) {
+				Piece.piece().forEach(function(row, i) {
 					row.forEach(function(cell, j) {
 						if (cell != '#FFF') {
 							var y = py + i;
@@ -135,13 +73,13 @@ define('state', [], function() {
 					});
 				});
 				py = 0;
-				pick_piece();
+				Piece.pick_piece(px, gwidth);
 			}
 		};
 
 		var coliding = function() {
 			var coliding = false;
-			piece.forEach(function(row, i) {
+			Piece.piece().forEach(function(row, i) {
 				row.forEach(function(cell, j) {
 					if (cell != '#FFF' && board[py + i][px + j] != '#FFF') {
 						coliding = true;
@@ -153,7 +91,7 @@ define('state', [], function() {
 		};
 					
 		var move_right = function() {
-			if (px + 1 + piece[0].length < gwidth) {
+			if (px + 1 + Piece.piece()[0].length < gwidth) {
 				px += 1;
 			}
 			if (coliding()) {
@@ -171,21 +109,19 @@ define('state', [], function() {
 			draw();
 		};
 
-
-
 		var rotate = function() {
-			current_rotation = ++current_rotation % piece_rotates.length;
-			piece = piece_rotates[current_rotation];
+			Piece.rotate();
+			if (px + Piece.piece()[0].length >= gwidth) {
+				px--;
+			}
 		};
-
-
 
 		this.board = board;
 		this.draw = draw;
 		this.update = update;
 		this.move_right = move_right;
 		this.move_left = move_left;
-		this.rotate = rotate;
+		this.rotate = Piece.rotate;
 	}
 	return {Game: Game};
 });
